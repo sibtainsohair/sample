9d9b078806b2b12d23d1cc79e7913ff4b1515dd5
Sibtain <sibtainsohair@hotmail.com>
2019-07-22 11:46:56 +0500

    2

--- src/main/java/com/example/posttest/CustomerController.java
+++ src/main/java/com/example/posttest/CustomerController.java
@@ -1,5 +1,8 @@
 package com.example.posttest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,10 +10,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CustomerController {
 	
+	@Autowired
+	private CustomerRepository cusrepo;
 	
 	@RequestMapping("/hi")
 	public String hi() {
 		return "hi";
+	}
+	
+	@RequestMapping("/customers")
+	public List<Customer> getcus() {
+		List<Customer> customers = new ArrayList<>();
+		cusrepo.findAll()
+		.forEach(customers::add);
+		return customers;
 	}
 
 }
