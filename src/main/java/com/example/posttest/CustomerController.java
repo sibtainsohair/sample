61a9a260136463624eed2da7e6536fa89e197f85
Sibtain <sibtainsohair@hotmail.com>
2019-07-22 12:03:46 +0500

    3

--- src/main/java/com/example/posttest/CustomerController.java
+++ src/main/java/com/example/posttest/CustomerController.java
@@ -2,8 +2,12 @@ package com.example.posttest;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+
+import javax.xml.bind.ParseConversionEvent;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,11 +23,17 @@ public class CustomerController {
 	}
 	
 	@RequestMapping("/customers")
-	public List<Customer> getcus() {
+	public List<Customer> getcustomers() {
 		List<Customer> customers = new ArrayList<>();
 		cusrepo.findAll()
 		.forEach(customers::add);
 		return customers;
+	}
+	
+	@RequestMapping("/customer/{id}")
+	public Optional<Customer> getcustomer(@PathVariable String id) {
+		int myid=Integer.parseInt(id);
+		return cusrepo.findById(myid);
 	}
 
 }
